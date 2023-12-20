8e1ca211b1531cf7843f5f3aa907b0049829fe90
NeedThisAccount <36775976+Retardeded@users.noreply.github.com>
2023-12-20 20:40:58 +0100

    Option to change password in profile is available.

--- src/main/java/pl/use/auction/controller/ProfileController.java
+++ src/main/java/pl/use/auction/controller/ProfileController.java
@@ -9,14 +9,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.use.auction.model.AuctionUser;
+import pl.use.auction.model.PasswordChangeResult;
 import pl.use.auction.repository.UserRepository;
+import pl.use.auction.service.UserService;
 
 @Controller
 public class ProfileController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/profile")
     public String viewProfile(Model model) {
@@ -37,6 +44,16 @@ public class ProfileController {
         return "profile-edit";
     }
 
+    @GetMapping("/profile/change-password")
+    public String showChangePasswordForm(Model model, Authentication authentication) {
+        String currentUserName = authentication.getName();
+        AuctionUser user = userRepository.findByEmail(currentUserName)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        model.addAttribute("user", user);
+        return "profile-change-password";
+    }
+
     @PostMapping("/profile/update")
     public String updateProfile(@ModelAttribute AuctionUser updatedUser, Authentication authentication) {
         String email = authentication.getName();
@@ -50,6 +67,34 @@ public class ProfileController {
         existingUser.setPhoneNumber(updatedUser.getPhoneNumber());
 
         userRepository.save(existingUser);
+
+        return "redirect:/profile";
+    }
+
+    @PostMapping("/profile/change-password")
+    public String changePassword(@RequestParam("oldPassword") String oldPassword,
+                                 @RequestParam("newPassword") String newPassword,
+                                 @RequestParam("confirmNewPassword") String confirmNewPassword,
+                                 Authentication authentication,
+                                 RedirectAttributes redirectAttributes) {
+        AuctionUser user = userRepository.findByEmail(authentication.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        PasswordChangeResult result = userService.changeUserPassword(oldPassword, newPassword, confirmNewPassword, user);
+
+        switch (result) {
+            case INVALID_OLD_PASSWORD:
+                redirectAttributes.addFlashAttribute("error", "The current password is incorrect.");
+                break;
+            case PASSWORD_MISMATCH:
+                redirectAttributes.addFlashAttribute("error", "The new passwords do not match.");
+                break;
+            case SUCCESS:
+                redirectAttributes.addFlashAttribute("message", "Password changed successfully!");
+                break;
+            default:
+                redirectAttributes.addFlashAttribute("error", "Password change failed.");
+        }
 
         return "redirect:/profile";
     }
--- src/main/java/pl/use/auction/service/UserService.java
+++ src/main/java/pl/use/auction/service/UserService.java
@@ -15,6 +15,7 @@ import pl.use.auction.dto.UserRegistrationDto;
 import pl.use.auction.exception.InvalidTokenException;
 import pl.use.auction.exception.TokenExpiredException;
 import pl.use.auction.model.AuctionUser;
+import pl.use.auction.model.PasswordChangeResult;
 import pl.use.auction.repository.UserRepository;
 
 import java.time.LocalDateTime;
@@ -76,6 +77,20 @@ public class UserService implements UserDetailsService {
         }
 
         return new User(auctionUser.getEmail(), auctionUser.getPassword(), new ArrayList<>());
+    }
+    @Transactional
+    public PasswordChangeResult changeUserPassword(String oldPassword, String newPassword, String confirmNewPassword, AuctionUser user) {
+        if (!passwordEncoder.matches(oldPassword, user.getPassword())) {
+            return PasswordChangeResult.INVALID_OLD_PASSWORD;
+        }
+
+        if (!newPassword.equals(confirmNewPassword)) {
+            return PasswordChangeResult.PASSWORD_MISMATCH;
+        }
+
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
+        return PasswordChangeResult.SUCCESS;
     }
 
     @Transactional
